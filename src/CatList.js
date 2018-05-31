4d0da3cd229c5a5d4de1df38e0a7307a09bb1d6e
theboymo <billfero@gmail.com>
2018-05-31 13:48:46 +0100

    Done.

--- src/CatList.js
+++ src/CatList.js
@@ -3,7 +3,7 @@ import React from 'react';
 const CatList = (props) => {
   const renderCats = props.catPics.map((cat, i) => 
     <li key={i} style={{width: '200px', marginRight: '10px'}}>
-      <img src={ cat.url } style={{ width: '100%'}}/>
+      <img src={ cat.url } style={{ width: '100%'}} alt="cat"/>
     </li>
   );
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import combinedReducer from './reducers';
-import { ConnectedApp } from './App'
+import { WrapperApp } from './App'
 
 const store = createStore(
   combinedReducer, applyMiddleware(thunk)
