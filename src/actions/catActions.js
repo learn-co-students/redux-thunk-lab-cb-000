19abb49031287c550a6c0868cf38c14df787335a
Jeremy Parker <jrobinparker@gmail.com>
2019-03-19 22:31:19 +0000

    Done.

---
+++ src/actions/catActions.js
@@ -0,0 +1,13 @@
+export function fetchCats () {
+  return (dispatch) => {
+    dispatch({type: 'LOADING_CATS'})
+    return fetch('http://localhost:4000/db')
+    .then(res =>  {
+      return res.json();
+    })
+    .then(resJson => dispatch({
+      type: 'FETCH_CATS',
+      payload: resJson.images
+    }))
+    }
+  }
